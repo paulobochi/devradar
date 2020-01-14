7847a0d5cffbae392e6c2aa87fccc97c5937d165
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2020-01-14 15:48:50 -0300

    Add filters to root devs query

--- backend/src/graphql/queries/root.js
+++ backend/src/graphql/queries/root.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
-const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
+const { 
+  GraphQLObjectType, 
+  GraphQLList, 
+  GraphQLID, 
+  GraphQLNonNull, 
+  GraphQLFloat, 
+  GraphQLString,
+} = require('graphql');
 
 const Dev = mongoose.model('dev');
 const DevType = require('../types/dev');
@@ -10,8 +16,33 @@ const RootQuery = new GraphQLObjectType({
   fields: () => ({
     devs: {
       type: new GraphQLList(DevType),
-      resolve() {
-        return Dev.find({});
+      args: { 
+        latitude: { type: GraphQLFloat }, 
+        longitude: { type: GraphQLFloat }, 
+        technologies: { type: new GraphQLList(GraphQLString) },
+      },
+      resolve(parentValue, { latitude, longitude, technologies }) {
+        const filters = {};
+
+        if (latitude && longitude) {
+          filters.location = {
+            $near: {
+              $geometry: {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+              },
+              $maxDistance: 10000,
+            },
+          };
+        };
+        
+        if (technologies && technologies.length > 0) {
+          filters.technologies = {
+            $in: technologies,
+          };
+        };
+
+        return Dev.find(filters);
       }
     },
     dev: {
